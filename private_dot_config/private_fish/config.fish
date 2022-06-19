1a9473b7bcef7d8872cc1afbfdbcfd27dab927e6
pwqbot <ding00000804@gmail.com>
2022-06-19 14:26:09 +0800

    add fish configure

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,22 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+alias mux "tmuxinator"
+alias cmo "chezmoi"
+alias gst "git st"
+alias gcm "git commit"
+alias gp "git push"
+alias gdog "git dog"
+alias gdag "git dag"
+alias gsb "git sb"
+alias gck "git checkout"
+alias gbr "git br"
+alias gbd "git bd"
+alias gres "git res"
+alias gup "git up"
+
+alias gdf "git diff"
+alias gwts "git wts"
+alias gwta "git wta"
+alias gwtr "git wtr"
